cbec7448c53a45ed1dd3fb279fd1877f4f1b1819
unknown <derek.slauson5@gmail.com>
2023-11-21 15:42:19 -0600

    Returning user dto with create ticket response.

--- MercuryApi/BLL/TicketBusinessLogic.cs
+++ MercuryApi/BLL/TicketBusinessLogic.cs
@@ -17,8 +17,15 @@ namespace MercuryApi.BLL
         public async Task<TicketDto> CreateTicket(TicketUpsert request)
         {
             Ticket ticket = _mapper.Map<Ticket>(request);
+
             await _repositoryManager.Ticket.CreateTicket(ticket);
             await _repositoryManager.SaveAsync();
+
+            if (request.UserId is int userId)
+            {
+                User? user = await _repositoryManager.User.GetUserById(userId);
+                ticket.User = user;
+            }
 
             return _mapper.Map<TicketDto>(ticket);
         }
